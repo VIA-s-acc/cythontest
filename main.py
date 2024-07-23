12dac6667b09e09029b5b436796fb7b225cb43a5
Georgii <99951336+VIA-s-acc@users.noreply.github.com>
2024-07-24 00:07:28 +0400

    rename

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from matrix_mult import c_sum, c_mult
+from matrix_methods import c_sum, c_mult
 res = c_sum([[1,2,3],[4,5,6]],[[1,2,3],[4,5,6]])
-res1 = c_mult([[1,2,3],[4,5,6], [1,2,3]],[[1,2,3],[4,5,6], [1,2,3]])
+res1 = c_mult([[1,0,0],[0,1,0], [0,0,1]],[[1,2,3],[4,5,6], [1,2,3]])
 
+print(res)
+print(res1)
